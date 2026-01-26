ce721070031afb9f36c48d7e81ecce25f345b167
yuvicc <yuvichh01@gmail.com>
2026-01-26 09:41:18 +0530

    git: added git and update git pull keymaps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -69,11 +69,12 @@ local plugins = {
     {
         'tpope/vim-fugitive',
         config = function()
-            -- Fugitive keymaps for common git operations
+            -- Git fugitive keymaps for common git operations
             vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git status' })
             vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = 'Git commit' })
             vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Git push' })
-            vim.keymap.set('n', '<leader>gl', ':Git pull<CR>', { desc = 'Git pull' })
+            vim.keymap.set('n', '<leader>gu', ':Git pull<CR>', { desc = 'Git pull' })
+            vim.keymap.set('n', '<leader>gl', ':Git log<CR>', { desc = 'Git log' })
             vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame' })
             vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', { desc = 'Git diff split' })
             vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { desc = 'Git add current file' })
